3564dfde7bc7a4e9cc160ffaccd1525acb37b1a2
vedant2709 <vedantchaudhary2002@gmail.com>
2025-06-21 22:08:28 +0530

    set sameSite to None

--- BACKEND/src/controllers/authController.js
+++ BACKEND/src/controllers/authController.js
@@ -69,7 +69,7 @@ export const loginUser = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // HTTPS only in production
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
